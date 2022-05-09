86a4c9cc3b34ef6bb20532da17f795577b87bf98
Mikayla Eckel Cifrese <mblessedmother@gmail.com>
2022-05-09 00:31:58 -0500

    Initial Commit of fresh version of project

---
+++ TraitClasses.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <string>
+#include <array>
+#include <iostream>
+#include <memory>
+#include "AnimalClass.h"
+
+using std::string;
+
+const static enum class Traits
+{
+	Furry, CommonPet, CanSwim, LivesUnderwater, CanRun, CanFly, Loyal, Arrogant, Mammal, Reptile, LivesOnLand,
+	NumTraits
+};
+
+class TraitType//base class defining basic 
+{
+	string m_Trait;
+
+	public:
+	TraitType(string Trait) : m_Trait{ Trait }
+	{
+	}
+
+	string Trait() const
+	{
+		return m_Trait;
+	}
+
+	virtual string Question() const = 0;
+};
+
+class Adjectives : public TraitType
+{
+	string Question() const override
+	{
+		return "Is it " + Trait() + "?";
+	}
+};
+
+class Verbs : public TraitType
+{
+	string Question() const override
+	{
+		return "Can it " + Trait() + "?";
+	}
+};
+
+class Nouns : public TraitType
+{
+	string Question() const override
+	{
+		return "Is it a " + Trait() + "?";
+	}
+};
+
+
+
+int main()
+{
+
+}
+
+
